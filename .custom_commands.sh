13d433e8c8bb71444b856d9635fa620e54b67766
Jordi Jaspers <jordi@Jordis-MacBook-Pro-2.local>
2019-09-26 00:37:44 +0200

    Can create folders and repo's now

--- .custom_commands.sh
+++ .custom_commands.sh
@@ -3,13 +3,13 @@
 # prints the input
 function create() {
     cd
-    python create.py $1
-    cd  "/Users/jordijaspers/Google\ Drive/Coding\ Projects/$1"
-    #git init
-    #git remote add origin git@github:34105935+Jordi-Jaspers@users.noreply.github.com/$1.git
-    #touch README.md
-    #git add .
-    #git commit -m "Initial Commit"
-    #git push -u origin master
-    #code .
+    python "/Users/jordi/Google Drive/Coding Projects/AutomaticProjectCreator/create.py" $1
+    cd  "/Users/jordi/Google Drive/Coding Projects/$1"
+    git init
+    git remote add origin git@github:34105935+Jordi-Jaspers@users.noreply.github.com/$1.git
+    touch README.md
+    git add .
+    git commit -m "Initial Commit"
+    git push -u origin master
+    code .
 }
---
+++ create.py
@@ -0,0 +1,18 @@
+import sys
+import os
+from github import Github
+
+PATH                = "/Users/jordi/Google Drive/Coding Projects/"
+USERNAME    = "Jordi-Jaspers"
+PASSWORD    =  "3620Gellik"
+
+def create():
+    folderName = str(sys.argv[1])
+    os.mkdir(PATH + str(folderName))
+
+    user = Github(USERNAME, PASSWORD).get_user()
+    repo = user.create_repo(folderName)
+    print("Successfully created repository & folder called -->{}".format(folderName))
+
+if __name__ == "__main__":
+    create()    
